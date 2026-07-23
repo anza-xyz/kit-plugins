161c751503b53f2a392fc4472ad0d9023e2a4d6e
Elliot Kennedy <me@elken.io>
2026-07-23 11:18:48 -0600

    [kit-plugin-litesvm] Add `GetProgramAccountsApi` support to litesvm Rpc

--- packages/kit-plugin-litesvm/test/litesvm-to-rpc.test.ts
+++ packages/kit-plugin-litesvm/test/litesvm-to-rpc.test.ts
@@ -1,4 +1,14 @@
-import { address, createClient, generateKeyPairSigner, lamports, Rpc } from '@solana/kit';
+import {
+    address,
+    Address,
+    Base58EncodedBytes,
+    createClient,
+    EncodedAccount,
+    generateKeyPairSigner,
+    getBase58Decoder,
+    lamports,
+    Rpc,
+} from '@solana/kit';
 import { describe, expect, it } from 'vitest';
 
 import { litesvmConnection as nodeLitesvm } from '../src/index';
@@ -165,6 +175,115 @@ describe('createRpcFromSvm', () => {
         expect(balance).toBe(lamports(1_000_000_000n));
     });
 
+    it('can fetch the accounts owned by a program', async () => {
+        const client = createClient().use(litesvm());
+        const [ownedA, ownedB, ownedOther] = await Promise.all([
+            generateKeyPairSigner().then(signer => signer.address),
+            generateKeyPairSigner().then(signer => signer.address),
+            generateKeyPairSigner().then(signer => signer.address),
+        ]);
+        const programAddress = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+        const otherProgram = address('11111111111111111111111111111111');
+
+        client.svm.setAccount(makeAccount(ownedA, new Uint8Array([1, 2, 3, 4]), programAddress));
+        client.svm.setAccount(makeAccount(ownedB, new Uint8Array([5, 6]), programAddress));
+        client.svm.setAccount(makeAccount(ownedOther, new Uint8Array([9]), otherProgram));
+
+        const accounts = await client.rpc.getProgramAccounts(programAddress, { encoding: 'base64' }).send();
+        expect(new Set(accounts.map(a => a.pubkey))).toStrictEqual(new Set([ownedA, ownedB]));
+        for (const { account } of accounts) {
+            expect(account.owner).toBe(programAddress);
+            expect(account.data[1]).toBe('base64');
+        }
+    });
+
+    it('applies a dataSize filter to the accounts owned by a program', async () => {
+        const client = createClient().use(litesvm());
+        const [addressA, addressB] = await Promise.all([
+            generateKeyPairSigner().then(signer => signer.address),
+            generateKeyPairSigner().then(signer => signer.address),
+        ]);
+        const programAddress = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+        client.svm.setAccount(makeAccount(addressA, new Uint8Array([1, 2, 3, 4]), programAddress));
+        client.svm.setAccount(makeAccount(addressB, new Uint8Array([1, 2]), programAddress));
+
+        const accounts = await client.rpc
+            .getProgramAccounts(programAddress, { encoding: 'base64', filters: [{ dataSize: 4n }] })
+            .send();
+        expect(accounts).toHaveLength(1);
+        expect(accounts[0].pubkey).toBe(addressA);
+    });
+
+    it('applies a memcmp filter to the accounts owned by a program', async () => {
+        const client = createClient().use(litesvm());
+        const [addressA, addressB] = await Promise.all([
+            generateKeyPairSigner().then(signer => signer.address),
+            generateKeyPairSigner().then(signer => signer.address),
+        ]);
+        const programAddress = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+        client.svm.setAccount(makeAccount(addressA, new Uint8Array([7, 8, 9, 10]), programAddress));
+        client.svm.setAccount(makeAccount(addressB, new Uint8Array([0, 0, 9, 10]), programAddress));
+
+        const bytes = getBase58Decoder().decode(new Uint8Array([9, 10])) as Base58EncodedBytes;
+        const accounts = await client.rpc
+            .getProgramAccounts(programAddress, {
+                encoding: 'base64',
+                filters: [{ memcmp: { bytes, encoding: 'base58', offset: 2n } }],
+            })
+            .send();
+        expect(new Set(accounts.map(a => a.pubkey))).toStrictEqual(new Set([addressA, addressB]));
+
+        const noMatch = await client.rpc
+            .getProgramAccounts(programAddress, {
+                encoding: 'base64',
+                filters: [{ memcmp: { bytes, encoding: 'base58', offset: 0n } }],
+            })
+            .send();
+        expect(noMatch).toHaveLength(0);
+    });
+
+    it('slices the data of the accounts owned by a program while preserving the full account space', async () => {
+        const client = createClient().use(litesvm());
+        const accountAddress = await generateKeyPairSigner().then(signer => signer.address);
+        const programAddress = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+        client.svm.setAccount(makeAccount(accountAddress, new Uint8Array([10, 20, 30, 40]), programAddress));
+
+        const accounts = await client.rpc
+            .getProgramAccounts(programAddress, { dataSlice: { length: 2, offset: 1 }, encoding: 'base64' })
+            .send();
+        expect(accounts).toHaveLength(1);
+        expect(accounts[0].account.data).toStrictEqual(['FB4=', 'base64']);
+        expect(accounts[0].account.space).toBe(4n);
+    });
+
+    it('throws when requesting a non-base64 encoding for the accounts owned by a program', async () => {
+        const client = createClient().use(litesvm());
+        const accountAddress = await generateKeyPairSigner().then(signer => signer.address);
+        const programAddress = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+        client.svm.setAccount(makeAccount(accountAddress, new Uint8Array([1, 2, 3, 4]), programAddress));
+
+        const promise = client.rpc.getProgramAccounts(programAddress, { encoding: 'base58' }).send();
+        await expect(promise).rejects.toThrow("Please use 'base64' encoding when using LiteSVM RPC");
+    });
+
+    it('wraps the accounts owned by a program result in a context when withContext is true', async () => {
+        const client = createClient().use(litesvm());
+        const accountAddress = await generateKeyPairSigner().then(signer => signer.address);
+        const programAddress = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+        client.svm.setAccount(makeAccount(accountAddress, new Uint8Array([1]), programAddress));
+
+        const response = await client.rpc
+            .getProgramAccounts(programAddress, { encoding: 'base64', withContext: true })
+            .send();
+        expect(typeof response.context.slot).toBe('bigint');
+        expect(response.value).toHaveLength(1);
+    });
+
     it('throws when calling an unsupported RPC method', async () => {
         const client = createClient().use(litesvm());
         const rpc = client.rpc as unknown as Rpc<{ unsupportedMethod: () => unknown }>;
@@ -172,3 +291,14 @@ describe('createRpcFromSvm', () => {
         await expect(promise).rejects.toThrow('Unsupported RPC method for LiteSVM client: unsupportedMethod');
     });
 });
+
+function makeAccount(address: Address, data: Uint8Array, programAddress: Address): EncodedAccount {
+    return {
+        address,
+        data,
+        executable: false,
+        lamports: lamports(1_000_000n),
+        programAddress,
+        space: BigInt(data.length),
+    };
+}
